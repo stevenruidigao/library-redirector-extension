ed6392d7546bbc8527d049508bf8c65d062c3a6a
Steven Gao <stevenruidigao@gmail.com>
2025-08-05 17:03:24 -0400

    Update maifest builder

--- buildManifest.js
+++ buildManifest.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 
 links = require('./links.json');
-matches = links.filter(link => link.name !== "LOCKSS (journal box)").map(link => link.url + "/*");
+
+excluded = [
+	"LOCKSS (journal box)",
+];
+matches = links.filter(link => !excluded.includes(link.name)).map(link => link.url + "/*");
 
 matches = matches.concat([
 	"https://dl.acm.org/*",
-])
+]);
 
 manifest = {
 	"name": "UMass Library Redirector Extension",
