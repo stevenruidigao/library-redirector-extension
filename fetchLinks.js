63d66dc97009e0d60705b58425e416ea142b7718
Steven Gao <stevenruidigao@gmail.com>
2025-08-07 00:50:31 -0400

    rework fetch script

--- fetchLinks.js
+++ fetchLinks.js
@@ -1,3 +1,5 @@
-links = Array.from((await (await fetch("https://login.silk.library.umass.edu/menu")).text()).matchAll(/<a href="http:\/\/silk.*?\/login\?url=((.*?:\/\/.*?)([\/?].*?)?)">(.*)<\/a>/g)).map((match) => ({ "full_url": match[1], "url": match[2], "name": match[4] }));
+domain = "silk.library.umass.edu";
+
+links = Array.from((await (await fetch(`https://login.${domain}/menu`)).text()).matchAll(/<a href="http:\/\/.*?\/login\?url=((.*?:\/\/.*?)([\/?].*?)?)">(.*)<\/a>/g)).map((match) => ({ "full_url": match[1], "url": match[2], "name": match[4] }));
 
 console.log(JSON.stringify(links).replaceAll("\\\\", "\\"));
